33428190b047ca0d9bacde4c9657a9896b28bc82
Anastasia Losikova <anast.losikova@gmail.com>
2022-03-25 20:51:07 +0300

    Initial Commit

---
+++ TZ_MobileUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TZ_MobileUp
+//
+//  Created by Анастасия Лосикова on 3/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
